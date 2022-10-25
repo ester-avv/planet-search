23b67b0f5af90aa7be7ee7400a6053a6ed77bec1
Ester Viana Verza <ester.av@hotmail.com>
2022-10-25 12:06:14 -0300

    requisito 1

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /* fetchApi(){
     const url = 'https://swapi.dev/api/planets';
 } */
 
 function Table() {
+  const [planetsList, setPlanetList] = useState([]);
+
+  useEffect(() => {
+    const getPlanets = async () => {
+      const endpoint = 'https://swapi.dev/api/planets';
+      const { results } = await fetch(endpoint).then((response) => response.json());
+      const filteredData = results.filter((e) => e !== e.residents);
+      setPlanetList(filteredData);
+    };
+    getPlanets();
+  }, []);
+
   return (
     <div>
       <table border="1">
@@ -26,9 +38,52 @@ function Table() {
             <th>URL</th>
           </tr>
         </thead>
-
         <tbody>
-          <h1>tabela</h1>
+          {
+            planetsList.map((e) => (
+              <tr key={ e.name }>
+                <td>
+                  {e.name}
+                </td>
+                <td>
+                  { e.rotation_period }
+                </td>
+                <td>
+                  { e.orbital_period }
+                </td>
+                <td>
+                  { e.diameter }
+                </td>
+                <td>
+                  { e.climate }
+                </td>
+                <td>
+                  { e.gravity }
+                </td>
+                <td>
+                  { e.terrain }
+                </td>
+                <td>
+                  { e.surface_water }
+                </td>
+                <td>
+                  { e.population }
+                </td>
+                <td>
+                  { e.films }
+                </td>
+                <td>
+                  { e.created }
+                </td>
+                <td>
+                  { e.edited }
+                </td>
+                <td>
+                  { e.url }
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </div>
