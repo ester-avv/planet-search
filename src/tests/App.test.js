975af404ffad4605593590328db00aeb9e4dfa48
Ester Viana Verza <ester.av@hotmail.com>
2022-10-28 10:10:13 -0300

    req testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
+import testData from "../../cypress/mocks/testData";import userEvent from "@testing-library/user-event";
+import AppProvider from '../Context/Provider'
 
-test('I am your test', () => {
+function renderWithContext(children) {
+  return(
+    render(
+      <AppProvider>
+        { children }
+      </AppProvider>
+    )
+  )
+}
+
+/* test('I am your test', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello, App!/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+describe('Verifica a requisição da API', () => {
+it('Testando a requisição feita no início da aplicação', async () => {
+  global.fetch = global.fetch = jest.fn().mockResolvedValue({
+    json: jest.fn().mockResolvedValue(testData),
+  })
+  await act( async () => {
+    renderWithContext(<App />);
+  })
+  const table = screen.getByRole('table')
+  expect(table).toBeInTheDocument();
+
+  const planet = screen.getByText('Tatooine')
+  expect (planet).toBeInTheDocument();
+
+  /* const columns = screen.findAllByRole('td');
+  expect(columns[0]).toBeInTheDocument(); */
+
 });
+it('Verifica filtros e tabela', async () => {
+  global.fetch = global.fetch = jest.fn().mockResolvedValue({
+    json: jest.fn().mockResolvedValue(testData),
+  })
+  await act( async () => {
+    renderWithContext(<App />);
+  })
+  const filterByColumn = screen.getByTestId('column-filter');
+    const filterByComparison = screen.getByTestId('comparison-filter');
+    const filterByAmount = screen.getByTestId('value-filter');
+    const btnToFilter = screen.getByTestId('button-filter');
+    const filterByName = screen.getByTestId('name-filter');
+
+    expect(filterByColumn).toBeInTheDocument();
+    expect(filterByComparison).toBeInTheDocument();
+    expect(filterByAmount).toBeInTheDocument();
+    expect(btnToFilter).toBeInTheDocument();
+
+    userEvent.type(filterByName, 'oo');
+    userEvent.selectOptions(filterByColumn,['rotation_period']);
+    userEvent.selectOptions(filterByComparison,['igual a']);
+    userEvent.type(filterByAmount, '23');
+
+    const planetT = screen.getByText('Tatooine');
+    expect(planetT).toBeInTheDocument();
+})
+})
